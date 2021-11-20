ee6a4a3da13e32a51b8985498247d84fa4294932
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-20 18:46:08 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryPic = galleryItems.map((galleryItem) =>
+`<a class="gallery__item" href="${galleryItem.original}">
+  <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>`).join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryPic);
+
+const openOriginal = function (event) {
+    const lightbox = new SimpleLightbox('.gallery a', {captionType: 'attr', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250});
+    
+    event.preventDefault();
+};
+
+gallery.addEventListener('click', openOriginal);
