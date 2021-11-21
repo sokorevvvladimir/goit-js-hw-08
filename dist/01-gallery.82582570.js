3f8cef1e7b55b218abe684e26b09d532acf68ba9
sokorevvvladimir <sokorevvvladimir@gmail.com>
2021-11-21 13:07:41 +0200

    adds almost done Task-3

--- dist/01-gallery.82582570.js
+++ dist/01-gallery.82582570.js
@@ -2071,7 +2071,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "4848" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8105" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+
+const formRef = document.querySelector('.feedback-form');
+const inputRef = document.querySelector('[name="email"]');
+const textAreaRef = document.querySelector('[name="message"]');
+
+formRef.addEventListener('input', throttle(callback, 500));
+
+function callback() {
+    localStorage.setItem("feedback-form-state", JSON.stringify({ "email": inputRef.value, "message": textAreaRef.value }));
+};
+
+formRef.addEventListener('submit', onSubmitHandler);
+
+function onSubmitHandler(e) {
+    e.preventDefault();
+    const {
+        elements: { email, message }
+    } = e.currentTarget;
+    
+    const curObj = { login: `${ email.value }`, message: `${ message.value }` }
+    console.log(curObj);
+    
+    localStorage.removeItem("feedback-form-state");
+    e.currentTarget.reset();
+}
+
+const currentLocalStorage = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+// (currentLocalStorage) ? inputRef = currentLocalStorage.email && textAreaRef = currentLocalStorage.message : null
+
 
